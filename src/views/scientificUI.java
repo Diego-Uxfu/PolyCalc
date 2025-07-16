5ff664aef441642cc4fd71e2f7259a9467c0efe6
Diego Mendez <dmendez.uo@gmail.com>
2025-07-16 16:56:46 -0700

    gives a starter button panel , currently incomplete

--- src/views/scientificUI.java
+++ src/views/scientificUI.java
@@ -1,4 +1,32 @@
 package views;
 
-public class scientificUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+import controllers.ButtonListener;
+
+public class scientificUI implements UserInterface {
+    @Override
+    public JPanel getButtonPanel(StringBuilder input, JTextField display){
+        JPanel buttonPanel = new JPanel(new GridLayout(5,5,5,5));
+
+        String[] buttonLabels = {
+                "x^-1", "(", ")", "/",
+                "7",    "8", "9", "*", // 7, 8, 9, multiplication
+                "4",    "5", "6", "-", // 4, 5, 6, subtract
+                "1",    "2", "3", "+", // 1, 2, 3, addition
+                "t",    "0", ".", "=" // tbd, 0, decimal, equal
+
+        };
+        for(String label: buttonLabels){ // shorthand for loop
+            JButton button = new JButton(label); // create button
+            button.setFont(new Font("Arial", Font.PLAIN, 12)); // making font
+            button.addActionListener(new ButtonListener(input, display)); // creating action listener per button
+            buttonPanel.add(button); // adding button to panel
+        }
+        return buttonPanel;
+    }
+
+
 }
