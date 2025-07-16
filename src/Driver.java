978a827566500feec7f2493536b9d39900f40e8e
Diego Mendez <dmendez.uo@gmail.com>
2025-07-16 16:57:06 -0700

    adds more modern look based off the OS

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,14 @@
+import javax.swing.*;
 import java.awt.*;
 
 public class Driver {
     public static void main(String[] args){
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
         Framework calc = new Framework();
     }
 }
