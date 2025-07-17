1d6238c79976cf26749ff399ab45736f5922654c
Diego Mendez <dmendez.uo@gmail.com>
2025-07-17 16:54:04 -0700

    updates case sensitivity

--- src/controllers/CalculatorSwitchListener.java
+++ src/controllers/CalculatorSwitchListener.java
@@ -7,17 +7,16 @@ import java.awt.event.ActionListener;
 import java.util.function.Consumer;
 
 public class CalculatorSwitchListener implements ActionListener {
-    private final UserInterface newui;
+    private final UserInterface newUI;
     private final Consumer<UserInterface> switchHandler;
 
-    public CalculatorSwitchListener(UserInterface newui, Consumer<UserInterface> switchHandler) {
-        this.newui = newui;
+    public CalculatorSwitchListener(UserInterface newUI, Consumer<UserInterface> switchHandler) {
+        this.newUI = newUI;
         this.switchHandler = switchHandler;
     }
 
-
     @Override
     public void actionPerformed(ActionEvent e){
-        switchHandler.accept(newui);
+        switchHandler.accept(newUI);
     }
 }
